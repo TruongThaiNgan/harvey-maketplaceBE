3c0b6101e1fffaa2644f63347fecf0d9362f6302
Otis <otis.nguyen.goldenowl@gmail.com>
2021-09-24 16:17:45 +0700

    feat: serve image

--- src/controllers/images.ts
+++ src/controllers/images.ts
@@ -2,5 +2,5 @@ import { NextFunction, Request, Response } from 'express';
 
 export const getImage = async (req: Request, res: Response, next: NextFunction) => {
   console.log(req.params);
-  return res.status(200).sendFile('../assets/boy.png');
+  return res.status(200).sendFile('../public/assets/images/boy.png');
 };
